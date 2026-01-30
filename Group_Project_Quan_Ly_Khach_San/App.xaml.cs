2432a30befdf53c5c0898fc47e97fe5f692963c0
deptraivodich <khatruong2k5@gmail.com>
2026-01-30 20:42:53 +0700

    Up load dự án

---
+++ Group_Project_Quan_Ly_Khach_San/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Group_Project_Quan_Ly_Khach_San
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
