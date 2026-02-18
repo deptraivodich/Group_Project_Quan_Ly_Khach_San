bdcdaf6a4941b1182d7b6ab9c628ee3f4a92a1ef
deptraivodich <khatruong2k5@gmail.com>
2026-02-18 14:07:07 +0700

    Dat Phong

---
+++ Group_Project_Quan_Ly_Khach_San/Dat_Phong/DanhSachDatPhong.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Group_Project_Quan_Ly_Khach_San.Dat_Phong
+{
+    public partial class DanhSachDatPhong : Window
+    {
+        public DanhSachDatPhong()
+        {
+            InitializeComponent();
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ Group_Project_Quan_Ly_Khach_San/Dat_Phong/DatPhong.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace Group_Project_Quan_Ly_Khach_San.Dat_Phong
+{
+    public partial class DatPhong : Window
+    {
+        public DatPhong()
+        {
+            InitializeComponent();
+            DataContext = new DatPhongViewModel();
+        }
+    }
+}
